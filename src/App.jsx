6ef37af43953f10819a3597601c291f6d97e5d1d
Joshua <horheyinc@gmail.com>
2024-11-23 11:37:43 +0100

    my h3

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Me and Teresas Project</h1>
       <h2>Ragnar says hi</h2>
       <h2>branching is easy!</h2>
+      <h3>last thing is pushing to your branch</h3>
     </>
   );
 }
